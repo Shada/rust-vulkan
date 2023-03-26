0895bb9d7a9b3a2464ba7cab07cea210b8960608
Tobias Andersson <siesta2007@hotmail.com>
2023-03-26 14:41:23 +0900

    list files

--- compile_shaders.sh
+++ compile_shaders.sh
@@ -4,5 +4,6 @@ git submodule update --init --recursive
 
 podman build --rm -t shaderc/shaderc -f shaderc/Dockerfile shaderc
 
+podman run --rm -v $PWD/assets/shaders:/code shaderc/shaderc ls
 podman run --rm -v $PWD/assets/shaders:/code shaderc/shaderc glslc shader.frag -o frag.spv
 podman run --rm -v $PWD/assets/shaders:/code shaderc/shaderc glslc shader.vert -o vert.spv
