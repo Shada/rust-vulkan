38a47a199ad89b11c8f31bf65fc0901cd0166062
Tobias Andersson <siesta2007@hotmail.com>
2023-03-26 15:47:42 +0900

    add windows script

--- compile_shaders.sh
+++ compile_shaders.sh
@@ -4,6 +4,5 @@ git submodule update --init --recursive
 
 podman build --rm -t shaderc/shaderc -f shaderc/Dockerfile shaderc
 
-podman run --rm -u $(id -u):$(id -g) -v $PWD/assets/shaders:/code shaderc/shaderc ls
 podman run --rm -u $(id -u):$(id -g) -v $PWD/assets/shaders:/code shaderc/shaderc glslc shader.frag -o frag.spv
 podman run --rm -u $(id -u):$(id -g) -v $PWD/assets/shaders:/code shaderc/shaderc glslc shader.vert -o vert.spv
