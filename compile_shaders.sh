7a6eacedbe53e3e621cf5567eebe382133a886bd
Tobias Andersson <siesta2007@hotmail.com>
2023-03-26 19:31:47 +0900

    testing

--- compile_shaders.sh
+++ compile_shaders.sh
@@ -8,8 +8,9 @@ git submodule update --init --recursive
 
 podman build --rm -t shaderc/shaderc -f shaderc/Dockerfile shaderc
 
-podman run --rm -u $(id -u):$(id -g) -v ./assets/shaders:/code shaderc/shaderc ls -la
-podman run --rm -u $(id -u):$(id -g) -v $PWD/assets/shaders:/code shaderc/shaderc ls -la
-podman run --rm -u $(id -u):$(id -g) -v $PWD/assets/shaders:/code shaderc/shaderc echo "$(id -u):$(id -g)"
-podman run --rm -u $(id -u):$(id -g) -v $PWD/assets/shaders:/code:rw shaderc/shaderc glslc shader.frag -o frag.spv
-podman run --rm -u $(id -u):$(id -g) -v $PWD/assets/shaders:/code:rw shaderc/shaderc glslc shader.vert -o vert.spv
+podman run --rm -u root:root -v ./assets/shaders:/code shaderc/shaderc ls -la
+podman run --rm -u root:root -v $PWD/assets/shaders:/code shaderc/shaderc ls -la
+podman run --rm -u root:root -v $PWD/assets/shaders:/code shaderc/shaderc echo "$(id -u):$(id -g)"
+podman run --rm -v $PWD/assets/shaders:/code shaderc/shaderc echo "$(id -u):$(id -g)"
+podman run --rm -u root:root -v $PWD/assets/shaders:/code:rw shaderc/shaderc glslc shader.frag -o frag.spv
+podman run --rm -u root:root -v $PWD/assets/shaders:/code:rw shaderc/shaderc glslc shader.vert -o vert.spv
