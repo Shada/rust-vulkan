096bd6942b76be419c4039cc8ab54a3cbe13a8fe
Tobias Andersson <siesta2007@hotmail.com>
2023-03-27 12:54:18 +0900

    utilize podman

--- compile_shaders.sh
+++ compile_shaders.sh
@@ -4,5 +4,7 @@ git submodule update --init --recursive
 
 podman pull shadahub/shaderc || podman build --rm -t shadahub/shaderc -f shaderc/Dockerfile shaderc
 
+podman unshare chown $(id -u):$(id -g) -R $PWD/assets/shaders
+
 podman run --rm -u $(id -u):$(id -g) -v $PWD/assets/shaders:/code:Z shadahub/shaderc glslc shader.frag -o frag.spv
 podman run --rm -u $(id -u):$(id -g) -v $PWD/assets/shaders:/code:Z shadahub/shaderc glslc shader.vert -o vert.spv
