827d1298475b65998924d18fa41d865e0e457d90
Tobias Andersson <siesta2007@hotmail.com>
2023-03-27 10:15:55 +0900

    make mapped volume rw

--- compile_shaders.sh
+++ compile_shaders.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-echo "$(id -u):$(id -g)"
-
-ls -la
-
 git submodule update --init --recursive
 
 podman build --rm -t shaderc/shaderc -f shaderc/Dockerfile shaderc
 
-podman run --rm -u root:root -v ./assets/shaders:/code shaderc/shaderc ls -la
-podman run --rm -u root:root -v $PWD/assets/shaders:/code shaderc/shaderc ls -la
-podman run --rm -u root:root -v $PWD/assets/shaders:/code shaderc/shaderc echo "$(id -u):$(id -g)"
-podman run --rm -v $PWD/assets/shaders:/code shaderc/shaderc echo "$(id -u):$(id -g)"
-podman run --rm -u root:root -v $PWD/assets/shaders:/code:rw shaderc/shaderc glslc shader.frag -o frag.spv
-podman run --rm -u root:root -v $PWD/assets/shaders:/code:rw shaderc/shaderc glslc shader.vert -o vert.spv
+podman run --rm -v $PWD/assets/shaders:/code:rw shaderc/shaderc glslc shader.frag -o frag.spv
+podman run --rm -v $PWD/assets/shaders:/code:rw shaderc/shaderc glslc shader.vert -o vert.spv
