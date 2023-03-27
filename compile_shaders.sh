2d3d004a25352802d72ef22a08b24fda66f9e52d
Tobias Andersson <siesta2007@hotmail.com>
2023-03-27 12:51:29 +0900

    utilize docker hub

--- compile_shaders.sh
+++ compile_shaders.sh
@@ -2,19 +2,7 @@
 
 git submodule update --init --recursive
 
-podman build --rm -t shaderc/shaderc -f shaderc/Dockerfile shaderc
+docker pull shadahub/shaderc || docker build --rm -t shadahub/shaderc -f shaderc/Dockerfile shaderc
 
-echo "permission test"
-
-echo "before"
-podman run --rm -u $(id -u):$(id -g) -v $PWD/assets/shaders:/code:Z shaderc/shaderc ls -la
-podman run --rm -v $PWD/assets/shaders:/code:Z shaderc/shaderc ls -la
-
-podman unshare chown $(id -u):$(id -g) -R $PWD/assets/shaders
-
-echo "after"
-podman run --rm -u $(id -u):$(id -g) -v $PWD/assets/shaders:/code:Z shaderc/shaderc ls -la
-podman run --rm -v $PWD/assets/shaders:/code:Z shaderc/shaderc ls -la
-
-podman run --rm -u $(id -u):$(id -g) -v $PWD/assets/shaders:/code:Z shaderc/shaderc glslc shader.frag -o frag.spv
-podman run --rm -u $(id -u):$(id -g) -v $PWD/assets/shaders:/code:Z shaderc/shaderc glslc shader.vert -o vert.spv
+docker run --rm -u $(id -u):$(id -g) -v $PWD/assets/shaders:/code:Z shadahub/shaderc glslc shader.frag -o frag.spv
+docker run --rm -u $(id -u):$(id -g) -v $PWD/assets/shaders:/code:Z shadahub/shaderc glslc shader.vert -o vert.spv
