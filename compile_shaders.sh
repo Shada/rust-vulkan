b78d26b5c6aa767e25467fd9ae4c9f21e8c8e290
Tobias Andersson <siesta2007@hotmail.com>
2023-03-27 11:42:28 +0900

    test unshare

--- compile_shaders.sh
+++ compile_shaders.sh
@@ -4,5 +4,17 @@ git submodule update --init --recursive
 
 podman build --rm -t shaderc/shaderc -f shaderc/Dockerfile shaderc
 
-podman run --rm -u $(id -u):$(id -g) -v $PWD/assets/shaders:/code:rw shaderc/shaderc glslc shader.frag -o frag.spv
-podman run --rm -u $(id -u):$(id -g) -v $PWD/assets/shaders:/code:rw shaderc/shaderc glslc shader.vert -o vert.spv
+echo "permission test"
+
+echo "before"
+podman run --rm -u $(id -u):$(id -g) -v $PWD/assets/shaders:/code:Z shaderc/shaderc ls -la
+podman run --rm -v $PWD/assets/shaders:/code:Z shaderc/shaderc ls -la
+
+podman unshare chown $(id -u):$(id -g) -R $PWD/assets/shaders
+
+echo "after"
+podman run --rm -u $(id -u):$(id -g) -v $PWD/assets/shaders:/code:Z shaderc/shaderc ls -la
+podman run --rm -v $PWD/assets/shaders:/code:Z shaderc/shaderc ls -la
+
+podman run --rm -u $(id -u):$(id -g) -v $PWD/assets/shaders:/code:Z shaderc/shaderc glslc shader.frag -o frag.spv
+podman run --rm -u $(id -u):$(id -g) -v $PWD/assets/shaders:/code:Z shaderc/shaderc glslc shader.vert -o vert.spv
